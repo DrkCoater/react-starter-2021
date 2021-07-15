83a26167a8058ebf47ca68c6659fb1256f64b91f
Jeremy Lu <zhillu@tesla.com>
2021-07-15 14:29:05 -0700

    remove extra dot

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     return (
         <div className="App">
             <h1> The count is: {count}</h1>
-            <button onClick={() => dispatch(increment())}>increment1</button>
+            <button onClick={() => dispatch(increment())}>increment</button>
             <button onClick={() => dispatch(decrement())}>decrement</button>
             <button onClick={() => dispatch(incrementBy(33))}>Increment by 33</button>
             <button onClick={() => dispatch(decrementBy(33))}>Decrement by 33</button>
