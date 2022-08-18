c9cf9e3987c364eda457eeadef9349f0803f6fd9
Jeremy Lu <zhillu@tesla.com>
2022-08-18 15:08:01 -0700

    upgrade to React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'regenerator-runtime/runtime'; // to use async/await (https://github.com/babel/babel/issues/9849)
 import React, { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-render(
+const root = createRoot(document.getElementById('app'));
+root.render(
     <StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </StrictMode>,
-    document.getElementById('app')
+    </StrictMode>
 );
