328510d0990f6be21a7a5601f84ae526cbb3a042
Jeremy Lu <zhillu@tesla.com>
2022-08-18 14:06:14 -0700

    merge with main

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, incrementBy, decrementBy } from './redux/counter';
-import ImgSrc from './assets/images/beautiful.jpg';
 
 export default function App() {
     const { count } = useSelector((state) => state.counter);
     const dispatch = useDispatch();
     return (
-        <div className="App">
-            <h1> The count is: {count}</h1>
-            <button onClick={() => dispatch(increment())}>increment</button>
-            <button onClick={() => dispatch(decrement())}>decrement</button>
-            <button onClick={() => dispatch(incrementBy(33))}>Increment by 33</button>
-            <button onClick={() => dispatch(decrementBy(33))}>Decrement by 33</button>
-            <br/><br/>
-            <img src={ImgSrc} />
+        <div>
+            <h1>Welcome to React App</h1>
+            <h3>Date : {new Date().toDateString()}</h3>
         </div>
     );
 }
